4265ffc4cdd7db0ade7002c59d8a106de053d4d2
Dennis Ong <dennisongwenghan@gmail.com>
2020-12-20 21:31:40 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string.h> 
+#include <map>
+using namespace std;
+
+void swap( int & x , int &y){ //3  //2 
+  int temp = y; // temp = 3
+  y = x; // y= 2
+  x = temp; // x = 3
+
+}
+
+
+int main() {
+    map<int, int> mp;
+  int count = 0;
+  int arr[8] = {2,6,5,0,8,7,1,3};
+  int pivot = arr[4];
+  
+
+  
+
+  
+}
